07d191a23ef500908214c284f4052ef2c1ac7477
Lucas Tamborim <lucastamborimdasilva@gmail.com>
2025-11-24 15:22:23 -0300

    Ajustes no models

--- anuncios/models.py
+++ anuncios/models.py
@@ -4,21 +4,31 @@ from django.db.models import Max
 
 class Anuncio(models.Model):
     owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name='anuncios')
-    marca = models.TextField(max_length=255)
-    modelo = models.TextField(max_length=255)
-    ano = models.IntegerField()
-    km = models.DecimalField(max_digits=10, decimal_places=2)
+    marca = models.CharField(max_length=255)
+    modelo = models.CharField(max_length=255)
+    ano = models.PositiveIntegerField()
+    km = models.PositiveIntegerField()
     preco = models.DecimalField(max_digits=10, decimal_places=2)
     telefone_contato = models.CharField(max_length=20)
     criado_em = models.DateTimeField(auto_now_add=True)
 
+    def __str__(self):
+        return f"{self.marca} {self.modelo} ({self.ano})"
+
+
 class Foto(models.Model):
     anuncio = models.ForeignKey(Anuncio, on_delete=models.CASCADE, related_name='fotos')
     imagem = models.ImageField(upload_to='fotos/')
-    ordem = models.IntegerField(editable=False, null=True, blank=True)
+    ordem = models.PositiveIntegerField(editable=False, null=True, blank=True)
+
+    class Meta:
+        ordering = ['ordem']
 
     def save(self, *args, **kwargs):
         if self.ordem is None:
             ultima_ordem = self.anuncio.fotos.aggregate(Max('ordem'))['ordem__max'] or 0
             self.ordem = ultima_ordem + 1
         super().save(*args, **kwargs)
+
+    def __str__(self):
+        return f"Foto {self.ordem} do Anuncio {self.anuncio.id}"
