1516546fab5d4a2c41975c7ec4ab755b96276073
Lucas Tamborim <lucastamborimdasilva@gmail.com>
2025-11-24 15:46:17 -0300

    Serializers adicionados

---
+++ anuncios/serializers.py
@@ -0,0 +1,40 @@
+from rest_framework import serializers
+from .models import Anuncio, Foto
+
+class FotoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Foto
+        fields = ['id', 'imagem', 'ordem']
+        read_only_fields = ['id', 'ordem'] #Campos que não podem ser alterados.
+
+class AnuncioListSerializer(serializers.ModelSerializer):
+    foto_principal = serializers.SerializerMethodField() #Criando campo para exibir apenas no anuncio
+
+    class Meta:
+        model = Anuncio
+        fields = ['id', 'marca', 'modelo', 'ano', 'preco', 'foto_principal']
+
+    def get_foto_principal(self, obj): #metodo customizado para buscar a primeira foto ( foto principal )
+        foto = obj.fotos.order_by('ordem').first() #pega a primeira foto relacionada a este anuncio
+        if foto:
+            return foto.imagem.url
+        return None
+
+class AnuncioDetailSerializer(serializers.ModelSerializer):
+    fotos = FotoSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Anuncio
+        fields = [
+            'id', 'marca', 'modelo', 'ano',
+            'km', 'preco', 'telefone_contato',
+            'criado_em', 'fotos'
+        ]
+
+class AnuncioCreateUpdateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Anuncio
+        fields = [
+            'marca', 'modelo', 'ano',
+            'km', 'preco', 'telefone_contato'
+        ]
