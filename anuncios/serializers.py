14a9182b2e6bdec857437edfdadf918badd6c222
Lucas Tamborim <lucastamborimdasilva@gmail.com>
2025-11-30 21:39:00 -0300

    Campo Owner adicionado nos serializers de anuncio.

--- anuncios/serializers.py
+++ anuncios/serializers.py
@@ -8,11 +8,12 @@ class FotoSerializer(serializers.ModelSerializer):
         read_only_fields = ['id', 'ordem'] #Campos que não podem ser alterados.
 
 class AnuncioListSerializer(serializers.ModelSerializer):
+    owner = serializers.CharField(source="owner.username", read_only=True)
     foto_principal = serializers.SerializerMethodField() #Criando campo para exibir apenas no anuncio
 
     class Meta:
         model = Anuncio
-        fields = ['id', 'marca', 'modelo', 'ano', 'preco', 'foto_principal']
+        fields = ['id', 'marca', 'modelo', 'ano', 'preco', 'foto_principal', 'owner']
 
     def get_foto_principal(self, obj): #metodo customizado para buscar a primeira foto ( foto principal )
         foto = obj.fotos.order_by('ordem').first() #pega a primeira foto relacionada a este anuncio
@@ -21,6 +22,7 @@ class AnuncioListSerializer(serializers.ModelSerializer):
         return None
 
 class AnuncioDetailSerializer(serializers.ModelSerializer):
+    owner = serializers.CharField(source="owner.username", read_only=True)
     fotos = FotoSerializer(many=True, read_only=True)
 
     class Meta:
@@ -28,7 +30,7 @@ class AnuncioDetailSerializer(serializers.ModelSerializer):
         fields = [
             'id', 'marca', 'modelo', 'ano',
             'km', 'preco', 'telefone_contato',
-            'criado_em', 'fotos'
+            'criado_em', 'fotos', 'owner'
         ]
 
 class AnuncioCreateUpdateSerializer(serializers.ModelSerializer):
